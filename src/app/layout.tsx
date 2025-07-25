6cec22a45dd80df51ace9179c2a0e816e4ad666f
silent-cipher <puspendramahariya@gmail.com>
2025-07-26 01:01:15 +0530

    fix linting in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        <script src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`} />
+        <script
+          async
+          src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
+        />
       </head>
       <body className={`${satoshiRegular.variable}`}>
         <Providers>
