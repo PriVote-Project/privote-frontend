acc76c79ff960fa024ff7eccea7ced83109751a2
silent-cipher <puspendramahariya@gmail.com>
2025-12-08 18:59:16 +0530

    refactor: update anonAadhaarVerifier contract addresses with prod key hash

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -12,7 +12,7 @@ export type ChainConstants = {
   chain: (typeof supportedChains)[number];
   contracts: {
     eas: Hex;
-    anonAadhaarVerifier: Hex;
+    anonAadhaarVerifier: Hex; // with Prod Key Hash
     semaphore: Hex;
     gitcoinPassportDecoder: Hex;
     zupassVerifier: Hex;
@@ -68,7 +68,7 @@ export const appConstants: Record<(typeof supportedChains)[number]['id'], ChainC
     chain: optimismSepolia,
     contracts: {
       eas: '0x4200000000000000000000000000000000000021',
-      anonAadhaarVerifier: '0x195CA63A04a22a552E3dDe8b7058B84D289a4FD6',
+      anonAadhaarVerifier: '0x276C6Ae425C58d84cE4540A4f5E8220A1ea486cc',
       semaphore: '0x697c80d1F2654e88d52B16154929EB976568DB04',
       gitcoinPassportDecoder: '0xe53C60F8069C2f0c3a84F9B3DB5cf56f3100ba56',
       zupassVerifier: '0x44ebd109487fbD1465Ca4cb035d0aFbf9f69884c'
@@ -108,7 +108,7 @@ export const appConstants: Record<(typeof supportedChains)[number]['id'], ChainC
     chain: baseSepolia,
     contracts: {
       eas: '0x4200000000000000000000000000000000000021',
-      anonAadhaarVerifier: '0x21046d78637897f667E1e3504A58777C0e9A95cc',
+      anonAadhaarVerifier: '0x63f6b6ff3FF4D986BCAde905514F651a06452f71',
       semaphore: '0x697c80d1F2654e88d52B16154929EB976568DB04',
       gitcoinPassportDecoder: '0x',
       zupassVerifier: '0x'
@@ -147,7 +147,7 @@ export const appConstants: Record<(typeof supportedChains)[number]['id'], ChainC
   //   chain: scrollSepolia,
   //   contracts: {
   //     eas: '0xaEF4103A04090071165F78D45D83A0C0782c2B2a',
-  //     anonAadhaarVerifier: '0x3e8baB4BD5c0e3A4f4a40AE6888913B9ea0A17B8',
+  //     anonAadhaarVerifier: '0x362554ed8BeeD359e2243eee5DA1865c6954C343',
   //     semaphore: '0x697c80d1F2654e88d52B16154929EB976568DB04',
   //     gitcoinPassportDecoder: '0x2443D22Db6d25D141A1138D80724e3Eee54FD4C2',
   //     zupassVerifier: '0x'
